4e6c9580b96574fd775219234e6890ad4cc3ff7a
nadia <nadia.torres@tecsup.edu.pe>
2025-03-29 19:11:50 -0500

    Laravel de Nadia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel de Nadia Torres!!
                 </div>
 
                 <div class="links">
